20a2fa65ccfd84e6d89400f6b348c05aee6e57e2
hornsbym <hornsbym19@mail.wlu.edu>
2018-05-17 16:16:52 -0400

    Added ship movement and click detection.

--- app/src/main/java/com/example/collinsherman/asteroidzgame/MainGamePanel.java
+++ app/src/main/java/com/example/collinsherman/asteroidzgame/MainGamePanel.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.animation.Animation;
 import android.widget.Toast;
 
 import com.example.collinsherman.asteroidzgame.model.Asteroid;
@@ -28,6 +30,11 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
     private int xClick = 0;
     private int yClick = 0;
 
+    private int shipX = 0;
+    private int shipY = 0;
+
+    private int shipPosX = 0;
+
     private MainThread thread;
     private Asteroid asteroid;
     private SpaceShip spaceship;
@@ -39,7 +46,10 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
         getHolder().addCallback(this);
         // Creates droid and loads bitmap
         asteroid = new Asteroid(BitmapFactory.decodeResource(getResources(), R.drawable.asteroid1), 50, 50);
-        spaceship = new SpaceShip(BitmapFactory.decodeResource(getResources(), R.drawable.ss_blue),  this.getWidth()/2, this.getHeight());
+        spaceship = new SpaceShip(BitmapFactory.decodeResource(getResources(), R.drawable.ss_blue),  400, 1000);
+        spaceship.setYSpeed(0);
+        Log.d(TAG, "ShipY: "+ shipY);
+        Log.d(TAG, "ShipX: "+ shipX);
         // Create game loop thread
         thread = new MainThread(getHolder(), this);
         // Allows handling of events
@@ -81,49 +91,69 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void update() {
+        int shipY = HEIGHT - (spaceship.getBitmap().getHeight()/2);
+        spaceship.setY(shipY);
         // Check collision with bottom
         if (asteroid.getSpeed().getYDir() == Speed.DIRECTION_DOWN && asteroid.getY() + asteroid.getBitmap().getHeight() / 2 <= 0) {
             //TODO: remove from screen
         }
+
+        if (spaceship.getSpeed().getXDir() == 1 && spaceship.getX() + spaceship.getBitmap().getWidth() / 2 >= WIDTH) {
+            spaceship.setXSpeed(0);
+        }
+
+        if (spaceship.getSpeed().getXDir() == -1 && spaceship.getX() - spaceship.getBitmap().getWidth() / 2 <= 0) {
+            spaceship.setXSpeed(0);
+        }
+
         asteroid.update();
+        spaceship.update();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            // delegating event handling to the droid
             xClick = (int)event.getX();
             yClick = (int)event.getY();
             Log.d(TAG, "Ship x: "+spaceship.getX());
             Log.d(TAG, "Ship y: "+spaceship.getY());
 
-            // check if in the lower part of the screen we exit
-            if (xClick > HEIGHT/2) {
+            if (xClick < spaceship.getX()+(spaceship.getBitmap().getWidth()/2) && xClick > spaceship.getX()-(spaceship.getBitmap().getWidth()/2)){
+                if (yClick < spaceship.getY()+(spaceship.getBitmap().getHeight()/2) && yClick > spaceship.getY()-(spaceship.getBitmap().getHeight()/2)){
+                    Log.d(TAG, "Settings clicked!");
+                }
+            }
+            if (xClick > WIDTH/2) {
                 Log.d(TAG, "Right side clicked!");
-            } else if (xClick < HEIGHT/2) {
+                spaceship.setXDirection(1);
+                spaceship.setXSpeed(10);
+            } else if (xClick < WIDTH/2) {
                 Log.d(TAG, "Left side clicked!");
+                spaceship.setXDirection(-1);
+                spaceship.setXSpeed(10);
             } else if (xClick == spaceship.getX() && yClick == spaceship.getY()) {
                 Log.d(TAG, "Ship clicked!");
             } else {
                 Log.d(TAG, "Coords: x=" + xClick + ",y=" + yClick);
             }
-        /*} if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            // the gestures
-            if (droid.isTouched()) {
-                // the droid was picked up and is being dragged
-                droid.setX((int)event.getX());
-                droid.setY((int)event.getY());
             }
-        } if (event.getAction() == MotionEvent.ACTION_UP) {
-            // touch was released
-            if (droid.isTouched()) {
-                droid.setTouched(false);*/
-            }
-        //}
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+           spaceship.setXSpeed(0);
+        }
         return true;
     }
 
-
+//    public getCanvasCoords(int bitmapDimensions, int xPos){
+//        int result = 0;
+//        int w = bitmapDimensions/2;
+//        int rightPoint = 0;
+//        int leftPoint = 0;
+//        rightPoint = xPos + w;
+//        leftPoint = xPos - w;
+//        return rightPoint, leftPoint;
+//    }
+//
     public void getViewSize(Canvas canvas){
         WIDTH = canvas.getWidth();
         HEIGHT = canvas.getHeight();
--- app/src/main/java/com/example/collinsherman/asteroidzgame/model/SpaceShip.java
+++ app/src/main/java/com/example/collinsherman/asteroidzgame/model/SpaceShip.java
@@ -3,6 +3,8 @@ package com.example.collinsherman.asteroidzgame.model;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
+import com.example.collinsherman.asteroidzgame.model.components.Speed;
+
 //import com.example.collinsherman.asteroidzgame.model.components.Speed;
 
 public class SpaceShip {
@@ -10,16 +12,22 @@ public class SpaceShip {
     private Bitmap bitmap;
     private int x;
     private int y;
-    //private Speed speed;
+    private Speed speed;
 
     public SpaceShip(Bitmap bitmap, int x, int y) {
         this.bitmap = bitmap;
         this.x = x;
         this.y = y;
-        //speed = new Speed();
+        speed = new Speed();
     }
 
-    //public Speed getSpeed() { return speed; }
+    public Speed getSpeed() { return speed; }
+
+    public void setXSpeed(int vel) { speed.setXVel(vel);}
+
+    public void setYSpeed(int vel) { speed.setYVel(vel);}
+
+    public void setXDirection(int dir) { speed.setXDir(dir);}
 
     public Bitmap getBitmap() { return bitmap; }
 
@@ -37,9 +45,9 @@ public class SpaceShip {
         canvas.drawBitmap(bitmap, x - (bitmap.getWidth() / 2), y - (bitmap.getHeight()/2), null);
     }
 
-    //public void update() {
-    //    x += (speed.getXVel());
-    //    y += (speed.getYVel() * speed.getYDir());
-    //}
+    public void update() {
+        x += (speed.getXVel() * speed.getXDir());
+        y += (speed.getYVel() * speed.getYDir());
+    }
 }
 
